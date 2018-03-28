a2a27373c634d3487cb09746f3869db5cbc699b4
Benedikt Schmaler <schmaler.benedikt@gmail.com>
2018-03-28 11:27:10 +0200

    fixed

--- src/main/java/ueb01/CharStackImpl.java
+++ src/main/java/ueb01/CharStackImpl.java
@@ -1,4 +1,34 @@
 package ueb01;
 
+import java.util.NoSuchElementException;
+
 class CharStackImpl implements CharStack {
+
+    private char [] ca = new char[0];
+
+    @Override
+    public void push(char c) {
+        char[] caNew = new char[ca.length +1];
+        System.arraycopy(ca, 0, caNew, 1, ca.length);
+        caNew[0] = c;
+        ca = caNew;
+    }
+
+    @Override
+    public char pop() {
+        if (ca.length == 0) {
+            throw new NoSuchElementException();
+        }
+
+        char[] caNew = new char[ca.length -1];
+        System.arraycopy(ca, 1, caNew, 0, ca.length -1);
+        char value = ca[0];
+        ca = caNew;
+        return value;
+    }
+
+    @Override
+    public int size() {
+        return ca.length;
+    }
 }
