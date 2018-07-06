2e9fe93e5ce563f4f2ef4589d1b860817d8fa45e
Bene2205 <schmaler.benedikt@gmail.com>
2018-07-06 13:47:34 +0200

    loesung Vorb.

--- src/main/java/ueb01/CharStackImpl.java
+++ src/main/java/ueb01/CharStackImpl.java
@@ -1,4 +1,61 @@
 package ueb01;
 
+import java.util.NoSuchElementException;
+
 class CharStackImpl implements CharStack {
+
+    class Element{
+        char v;
+        Element next;
+
+        public Element(char v, Element next) {
+            this.v = v;
+            this.next = next;
+        }
+    }
+
+    Element head = null;
+    int size = 0;
+
+    @Override
+    public void push(char c) {
+        if (head == null) {
+            head = new Element(c, null);
+            size = 1;
+            return;
+        }
+        Element it = head;
+        while(it.next != null) {
+            it = it.next;
+        }
+        it.next = new Element(c, null);
+        size = size +1;
+    }
+
+    @Override
+    public char pop() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        if (size == 1) {
+            char r = head.v;
+            head = null;
+            size--;
+            return r;
+        }
+
+        Element it = head;
+        for(int i = 0; i < size-2; i++) {
+            it = it.next;
+        }
+        char r = it.next.v;
+        it.next = null;
+        size--;
+        return r;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
 }
--- src/main/java/ueb01/Palindrom.java
+++ src/main/java/ueb01/Palindrom.java
@@ -3,7 +3,33 @@ package ueb01;
 class Palindrom {
 	static boolean istPalindrom(String s) {
 		// Verwenden Sie die Klasse CharStackImpl um auf Palindrom zu testen!
+		s = s.trim();
+		s = s.toLowerCase();
+		char[] pali = s.toCharArray();
+		CharStack cs = new CharStackImpl();
 
-		return false;
+		//länge 1 immer true
+		if (pali.length == 1) return true;
+
+		// wenn ungerade länge ignoriere element in der mitte
+		if (pali.length % 2 != 0) {
+			for (int i = (pali.length % 2); i < pali.length - 1; i++) {
+				cs.push(pali[i]);
+			}
+		} else {
+			// füge 2 hälfte des arrays in charstack
+			for (int i = (pali.length % 2) - 1; i < pali.length - 1; i++) {
+				cs.push(pali[i]);
+			}
+		}
+		//prüfe ob erste hälfte mit charstack übereinstimmt
+		for (int i = 0; i < pali.length % 2; i++){
+			if (pali[i] != cs.pop()) {
+				return false;
+			}
+		}
+
+		// alles passt
+		return true;
 	}
 }
